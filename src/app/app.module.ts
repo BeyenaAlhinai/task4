d9d39553e8b96551c3c285b93ad8268603b25ee5
BeyenaAlhinai <Beyenaalhinai@gmail.com>
2023-09-20 15:53:21 +0400

    Develop a Questionnaire Port

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { ResultComponent } from './result/result.component';
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    ResultComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
